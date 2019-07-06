e3f6729d25ac7d3734e45a22495e44b48f8775ed
mainakmanna <moinak.manna@gmail.com>
2019-07-06 09:09:34 +0530

    Code cleanup.

--- mdfile-generator.py
+++ mdfile-generator.py
@@ -5,12 +5,11 @@ http = urllib3.PoolManager()
 URL_CONTAINER_FILE = 'url_list_file'
 with open(URL_CONTAINER_FILE, 'rb') as f:
     contents = f.read()
-#contents = contents.decode("ISO-8859-1")
+
 contents = contents.decode("utf-8")
 # you may also want to remove whitespace characters like `\n` at the end of each line
 contents = contents.split('\n')
-#print(contents)
-#contents.sort()
+
 OUTPUT = r"./output"
 if os.path.exists(OUTPUT) and os.path.isdir(OUTPUT):
     print('INFO: DIrectory Exists')
@@ -24,22 +23,23 @@ for url in contents:
     if len(url) > 0 :
         url = url.replace('\r','')
         print(url)
+        # HTTP request to the URL which points to the transcript page.
         response = http.request('GET', url+'/lm')
         soup = BeautifulSoup(response.data, features="html.parser")
+        # getting to the correct div containing the transcript.
         p = soup.find_all("div",class_="richtext-content-container")
-        s = p[0].get_text()
-        #print(s)
+        
+        s  = ""
+        if len(p) > 0:
+            s = p[0].get_text()
+        
         s = s.splitlines()
-        #print(url.split('/'))
+        
         filename = url.split('/')[-2]+'.md'
         with open(str(index)+'-'+filename,'w', encoding="utf-8")as text_file:
-            #text_file.write('|DE|EN|\n')
-            #text_file.write('|---|---|\n')
             
             for line in s:
                 if len(line) > 0 :
-                    #text_file.write('|'+line+'||\n')
                     text_file.write((line+'\n'))
             text_file.close()
-        #break
     index = index + 1
